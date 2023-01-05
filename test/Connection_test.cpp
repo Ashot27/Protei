a0fd69b47f66455e58282681022a830e88baf3dc
Ashot Avetisian <ashot463@mail.ru>
2023-01-05 12:16:07 +0300

    connection test

---
+++ test/Connection_test.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+#include "../client/Client.h"
+#include "../server/Server.h"
+
+class Connection_test : public ::testing::Test {
+   protected:
+    Connection_test() {}
+
+    ~Connection_test() override {}
+
+    void SetUp() override {}
+
+    void TearDown() override {}
+
+   private:
+};
+
+#define IP "127.0.0.1"
+#define PORT 8080
+
+TEST(Connection_test, udp_client_prepare) {
+    Client client(IP, PORT, false);
+    Client_status _status = client.prepare();
+    EXPECT_TRUE(client.is_status_ok());
+    client.stop();
+}
+
+TEST(Connection_test, tcp_client_prepare_without_server) {
+    Client client(IP, PORT, true);
+    Client_status _status = client.prepare();
+    EXPECT_FALSE(client.is_status_ok());
+    client.stop();
+}
+
+TEST(Connection_test, tcp_client_prepare_with_server) {
+    Server server = Server::get_instance(IP, PORT);
+    Server_status s_status = server.prepare();
+    EXPECT_TRUE(s_status == Server_status::up);
+    Client client(IP, PORT, true);
+    Client_status c_status = client.prepare();
+    EXPECT_TRUE(client.is_status_ok());
+    client.stop();
+    server.stop();
+}
